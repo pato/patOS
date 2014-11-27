47597d7470f9b018debc10fe9421561501fd2505
Pato <plankenau@gmail.com>
2014-11-27 13:38:04 -0600

    forgot to clear the screen before rearranging windows

--- kernel/windowmanager.cc
+++ kernel/windowmanager.cc
@@ -1,31 +1,49 @@
 #include "windowmanager.h"
 
-/**
- * Assumptions: 
- * splits is the number of windows accross that axis (starts at 1)
- * which is the index of the window (also starts at 1)
- *
- */
 
 Layout*** WindowManager::layouts;
+Window* WindowManager::backdrop;
+VGA WindowManager::vga;
+
 
 void WindowManager::init() {
   layouts = new Layout**[5];
   layouts[0] = nullptr;
-  layouts[1] = new Layout*[1] { new Layout(1,1) };
-  layouts[2] = new Layout*[2] { new Layout(2,1), new Layout(2,2)};
+  layouts[1] = new Layout*[1] {new Layout(1,1,1,1)};
+  layouts[2] = new Layout*[2] {new Layout(1,2,1,1),new Layout(1,2,1,2)};
+  layouts[3] = new Layout*[3] {new Layout(1,2,1,1),new Layout(2,2,1,2),new Layout(2,2,2,2)};
+  layouts[4] = new Layout*[4] {new Layout(2,2,1,1),new Layout(2,2,2,1),new Layout(2,2,1,2),new Layout(2,2,2,2)};
+  layouts[5] = new Layout*[5] {new Layout(2,2,1,1),new Layout(2,2,2,1),new Layout(3,2,1,2),new Layout(3,2,2,2),new Layout(3,2,3,2)};
+
+  backdrop = new Window(vga, "PatOS - version 0.0.0.0.1", 0, 0, 25, 80, VGA::GREEN, VGA::WHITE);
 }
 
 void WindowManager::addWindow(const char* name, int fg) {
+  addWindow(name, DEFAULTBG, fg);
+}
+
+void WindowManager::addWindow(const char* name, int bg, int fg) {
   int winCount = windowMap.getSize();
 
+
+  backdrop->clear();
   windowMap.lock();
   MapNode<Window>* curr = windowMap.head;
-  for (int i = 0; curr != nullptr; i++) {
-    if (i >= winCount) Debug::panic("found more windows while iterating than winCount");
+  int i = 0;
+  for (; curr != nullptr; i++) {
+    if (i >= winCount) Debug::panic("found more windows than winCount while iterating");
 
     curr->value->resize(layouts[winCount + 1][i]);
     curr = curr->next;
   }
   windowMap.unlock();
+
+  if (i != winCount) Debug::panic("i != winCount after iterating over windowMap");
+
+  Layout* l = layouts[winCount + 1][i];
+  Window* newWin = new Window(vga, name, l->r, l->c, l->h, l->w, bg, fg);
+
+  int id = Process::current ? Process::current->id : 666;
+
+  windowMap.add(id, newWin);
 }
