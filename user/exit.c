eb2e03dd85f60147e7f822d56b2c724a801cad46
Pato <plankenau@gmail.com>
2014-12-07 12:18:34 -0600

    fixed exit code number

--- user/exit.c
+++ user/exit.c
@@ -4,9 +4,7 @@ int main(int argc, char** argv) {
   if (argc == 1) { 
     exit(0);
   } else {
-    puts("about to exit with status code: ");
-    putdec((long)argv[1] - '0');
-    //exit((long)argv[1] - '0');
+    exit((int)(argv[1][0] - '0'));
   }
   return 0;
 }
