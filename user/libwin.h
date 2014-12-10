76e0dfdd8b8fdb561ae98ae127282be35e8f5c11
Pato <plankenau@gmail.com>
2014-12-10 12:11:06 -0600

    added setting of colors

--- user/libwin.h
+++ user/libwin.h
@@ -8,20 +8,20 @@ extern long win_cursor(int r, int c);
 extern long win_clear();
 extern long win_size(int* w, int* h);
 
-#define BLACK 0;
-#define BLUE 1;
-#define GREEN 2;
-#define CYAN 3;
-#define RED 4;
-#define MAGENTA 5;
-#define BROWN 6;
-#define LIGHT_GRAY 7;
-#define DARK_GRAY 8;
-#define LIGHT_BLUE 9;
-#define LIGHT_GREEN 10;
-#define LIGHT_CYAN 11;
-#define LIGHT_RED 12;
-#define LIGHT_MAGENTA  13;
-#define LIGHT_BROWN 14;
-#define WHITE 15;
+#define BLACK 0
+#define BLUE 1
+#define GREEN 2
+#define CYAN 3
+#define RED 4
+#define MAGENTA 5
+#define BROWN 6
+#define LIGHT_GRAY 7
+#define DARK_GRAY 8
+#define LIGHT_BLUE 9
+#define LIGHT_GREEN 10
+#define LIGHT_CYAN 11
+#define LIGHT_RED 12
+#define LIGHT_MAGENTA  13
+#define LIGHT_BROWN 14
+#define WHITE 15
 #endif
