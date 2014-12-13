10fbc9b93d87bb906d96557099bbff3292208ab7
Pato <plankenau@gmail.com>
2014-12-13 02:06:48 -0600

    prefilled keyboard buffer with welcome

--- kernel/init.cc
+++ kernel/init.cc
@@ -4,6 +4,7 @@
 #include "fs.h"
 #include "libk.h"
 #include "process.h"
+#include "kbd.h"
 
 Init::Init() : Process("init",nullptr) {
 }
@@ -17,6 +18,14 @@ long Init::run() {
 
   SimpleQueue<const char*> argv;
   argv.addTail(K::strdup("shell"));
+  Keyboard::bb->put('w');
+  Keyboard::bb->put('e');
+  Keyboard::bb->put('l');
+  Keyboard::bb->put('c');
+  Keyboard::bb->put('o');
+  Keyboard::bb->put('m');
+  Keyboard::bb->put('e');
+  Keyboard::bb->put(13);
   execv("shell",&argv,1);
 
   Debug::shutdown("What?");
